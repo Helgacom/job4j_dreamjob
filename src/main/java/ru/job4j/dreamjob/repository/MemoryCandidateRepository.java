a531b22069ae17ec8f8878a46f8c74fd26c64af5
Olga Remneva <remneva_olga@mail.ru>
2024-03-21 12:21:18 +0400

    Vacancy view, edit, delete

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -38,9 +38,8 @@ public class MemoryCandidateRepository implements CandidateRepository {
         return candidate;
     }
 
-    @Override
-    public void deleteById(int id) {
-        candidates.remove(id);
+    public boolean deleteById(int id) {
+        return candidates.remove(id) != null;
     }
 
     @Override
